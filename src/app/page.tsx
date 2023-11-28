3c14b72ef9c4ffc1d7e3b3310941dd10265e2ce6
gumelarme <gumelar.pn@gmail.com>
2023-11-29 02:20:19 +0800

    Feat: Add ZHChar components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,9 @@
 import Image from "next/image";
 import {useEffect, useState} from "react";
 import { useDebounce } from 'use-debounce'
+import { ZHChar } from '../components/ZHChar'
 
-// <code className={`text-2xl ${textShownFont}`}>郑</code>
-// <code className={`text-l ${textShownFont}`}>zheng</code>
-
-// <input
-//     id="email"
-//     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
-//                 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-//                 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border-blue-1000"
-//     placeholder="john.doe@company.com"
-//     required
-// />
-
+// TODO: Apply cosmetics
 export default function Home() {
     const textShownFont = "flex justify-center font-mono font-bold text-violet-600";
     const visibilityMode = [
@@ -27,20 +17,28 @@ export default function Home() {
     const [curVisibility, setCurVisibility] = useState(visibilityMode[1].key);
     const [inputText, setInputText] = useState("");
     const [debouncedInputText] = useDebounce(inputText, 500);
-    const [inputLength, setinputLength] = useState(1);
+    const [zhText, setZHText] = useState([
+        {zh: "你", pinyin: "ni3", visible: true},
+    ]);
 
     useEffect(() => {
-        setinputLength(debouncedInputText.length)
+        //TODO: Fetch data, then set zhText
     }, [debouncedInputText])
 
     return (
         <main className="h-screen flex flex-col text-2xl text-white">
-            <section className="h-3/5 min-h-[10rem] bg-gray-800">
-                <p> {inputLength} characters </p>
+            <section className="h-3/5 min-h-[10rem] bg-gray-800 p-4 overflow-y-scroll">
+                <div className="flex flex-wrap">
+                    {/*TODO: Adjust visibility based on mode*/}
+                    {zhText.map((item, i) => (
+                        <ZHChar zh={item.zh} pinyin={item.pinyin} is_visible={item.visible} key={i}/>
+                    ))}
+                </div>
             </section>
             <section className="flex flex-col flex-grow min-h-[8rem] md:h-80">
                 <div className="flex text-white text-lg bg-blue-900 items-center py-1 px-2">
                     <div className="">
+                        {/* TODO: Fetch data from /texts */}
                         <select name="Preset" id="" className="bg-gray-800 p-1 bg-white text-black" >
                             <option value="" disabled selected hidden>Presets</option>
                             <option value="">HSK 1</option>
---
+++ src/components/ZHChar.tsx
@@ -0,0 +1,28 @@
+
+export function ZHChar(props: {zh: string, pinyin: string, is_visible: boolean}) {
+    // TODO: Change the behaviour so that it shows lexeme as unit, instead of each char
+    // Example:
+    //   美国人     vs   美   国   人
+    // meiguoren       mei  guo  ren
+
+    // TODO: Pick better colors
+    const colors = [
+        "red", "limegreen", "rebeccapurple", "purple", "slategray"
+    ];
+
+    const tone = parseInt(props.pinyin.slice(-1));
+    const color = colors[tone-1];
+
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center px-2">
+                <span style={{color}}>
+                    {props.zh}
+                </span>
+                <span className={props.is_visible ? "visible" : "invisible"}>
+                    {props.pinyin.slice(0, -1)}
+                </span>
+            </div>
+        </>
+    );
+}
