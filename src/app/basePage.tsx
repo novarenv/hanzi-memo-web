cc3f6f88f383a9e693bcd16d4d37334f9b9210be
novarenv <novarenv@gmail.com>
2023-11-28 09:50:38 +0800

    Maaf mass T.T

--- src/app/basePage.tsx
+++ src/app/basePage.tsx
@@ -1,95 +1,42 @@
-import Image from "next/image";
-import { useEffect } from "react";
+import Image from 'next/image'
 
-// async function getData() {
-//   const res = await fetch('https://hz.youcantry.me/api/pinyins/%E6%84%9B')
-//   // The return value is *not* serialized
-//   // You can return Date, Map, Set, etc.
- 
-//   if (!res.ok) {
-//     // This will activate the closest `error.js` Error Boundary
-//     throw new Error('Failed to fetch data')
-//   }
- 
-//   return res.json()
-// }
-
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch(`https://.../data`)
-  const data = await res.json()
- 
-  // Pass data to the page via props
-  return { props: { data } }
-}
-
-export default async function Home() {
-  const textShownFont = "flex justify-center font-mono font-bold text-violet-600"
-
-  const data = await getServerSideProps()
-
-  useEffect(() => {
-    console.log('Data', data)
-  }, [])
-    
-
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between l:p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div
-          className="flex flex-end h-48 w-full items-end justify-center bg-gradient-to-t from-white
-            via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"
-        >
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.tsx</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            {/* <Image
+            By{' '}
+            <Image
               src="/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
               height={24}
               priority
-            /> */}
-            <div>Show</div>
-            <div>Collections</div>
+            />
           </a>
         </div>
       </div>
 
-      {/* <div
-        className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px]
-          before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent
-          before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3
-          after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br
-          before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff]
-          after:dark:opacity-40 before:lg:h-[360px] z-[-1]"
-      ></div> */}
-
-      <div>
-        <div className="mb-6">
-          <p
-            className="flex flex-col w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200
-                pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto
-                rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 mb-4"
-          >
-            {/* <p className=""> */}
-              <code className={`text-2xl ${textShownFont}`}>郑</code>
-              <code className={`text-l ${textShownFont}`}>zheng</code>
-            {/* </p> */}
-          </p>
-          <input
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
-                focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-                dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border-blue-1000"
-            placeholder="john.doe@company.com"
-            required
-          />
-        </div>
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -100,7 +47,7 @@ export default async function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
+            Docs{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -117,7 +64,7 @@ export default async function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
+            Learn{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -134,7 +81,7 @@ export default async function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
+            Templates{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -151,7 +98,7 @@ export default async function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
+            Deploy{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -162,5 +109,5 @@ export default async function Home() {
         </a>
       </div>
     </main>
-  );
+  )
 }
