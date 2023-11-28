8b2a9011174ec988ff68008501719aa8cb826386
gumelarme <gumelar.pn@gmail.com>
2023-11-29 01:11:32 +0800

    Feat: control layout placement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from "next/image";
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 
 // <code className={`text-2xl ${textShownFont}`}>郑</code>
 // <code className={`text-l ${textShownFont}`}>zheng</code>
@@ -16,23 +16,50 @@ import { useEffect } from "react";
 // />
 
 export default function Home() {
-  const textShownFont = "flex justify-center font-mono font-bold text-violet-600";
-  const inputText: string = "";
+    const textShownFont = "flex justify-center font-mono font-bold text-violet-600";
+    const inputText: string = "";
+    const visibilityMode = [
+        {key: "show_all", label: "Show All"},
+        {key: "smart", label: "Smart"},
+        {key: "hide_all", label: "Hide All"},
+    ];
 
-  useEffect(() => {}, [])
+    const [curVisibility, setCurVisibility] = useState(visibilityMode[1].key);
 
-  return (
-    <main className="h-screen flex flex-col text-2xl text-white">
-      <section className="h-1/2 min-h-[10rem] bg-gray-800 md:flex-grow">
-      </section>
-      <section className="flex-grow min-h-[10rem] md:flex-initial md:h-80">
-          <textarea
-              className="h-full w-full bg-gray-900 p-4"
-              placeholder="Write something here"
-              value={inputText}
-              readOnly={true}>
-          </textarea>
-      </section>
-    </main>
-  );
+    return (
+        <main className="h-screen flex flex-col text-2xl text-white">
+            <section className="h-3/5 min-h-[10rem] bg-gray-800">
+            </section>
+            <section className="flex flex-col flex-grow min-h-[8rem] md:h-80">
+                <div className="flex text-white text-lg bg-blue-900 items-center py-1 px-2">
+                    <div className="">
+                        <select name="Preset" id="" className="bg-gray-800 p-1 bg-white text-black" >
+                            <option value="" disabled selected hidden>Presets</option>
+                            <option value="">HSK 1</option>
+                            <option value="">HSK 2</option>
+                            <option value="">Intermediate</option>
+                        </select>
+                    </div>
+                    <div className="flex flex-row-reverse flex-grow">
+                        {visibilityMode.map((x, i) => (
+                            <label
+                                className={`flex gap-2 py-1 px-2 hover:bg-gray-400 hover:cursor-pointer ${(curVisibility == x.key ? "bg-gray-200 text-black font-bold" : "")}`}
+                                key={i}>
+                                <input type="radio" name="visibility-toggle" onChange={() => setCurVisibility(x.key)}
+                                       hidden={true}/>
+                                <span>
+                          {x.label}
+                      </span>
+                            </label>
+                        ))}
+                    </div>
+                </div>
+                <textarea
+                    className="flex-grow w-full bg-gray-900 p-4"
+                    placeholder="Write something here"
+                    defaultValue={inputText}>
+                </textarea>
+            </section>
+        </main>
+    );
 }
