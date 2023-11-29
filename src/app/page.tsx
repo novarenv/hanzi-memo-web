22186c9149f120f129ef01b26c403e832ebc369d
gumelarme <gumelar.pn@gmail.com>
2023-11-29 21:06:07 +0800

    Feat: pinyin with marked tones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,107 +1,107 @@
 "use client";
 
-import { AppInitialProps } from "next/app";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import { useDebounce } from "use-debounce";
-import { ZHChar } from "../components/ZHChar";
-import { GET } from "../app/api/route";
+import {useEffect, useState} from "react";
+import {useDebounce} from "use-debounce";
+import {ZHChar} from "../components/ZHChar";
+import {GET} from "../app/api/route";
 
 // TODO: Apply cosmetics
 export default function Home() {
-  const textShownFont =
-    "flex justify-center font-mono font-bold text-violet-600";
-  const visibilityMode = [
-    { key: "show_all", label: "Show All" },
-    { key: "smart", label: "Smart" },
-    { key: "hide_all", label: "Hide All" },
-  ];
+    const textShownFont =
+        "flex justify-center font-mono font-bold text-violet-600";
+    const visibilityMode = [
+        {key: "show_all", label: "Show All"},
+        {key: "smart", label: "Smart"},
+        {key: "hide_all", label: "Hide All"},
+    ];
 
-  const [curVisibility, setCurVisibility] = useState(visibilityMode[1].key);
-  const [inputText, setInputText] = useState("");
-  const [debouncedInputText] = useDebounce(inputText, 500);
-  const [inputLength, setinputLength] = useState(1);
-  const [zhText, setZHText] = useState([
-    { zh: "你", pinyin: "ni3", visible: true },
-  ]);
-  const [get, setGet] = useState([]);
+    const [inputLength, setinputLength] = useState(1);
+    const [get, setGet] = useState([]);
+    const [curVisibility, setCurVisibility] = useState(visibilityMode[1].key);
+    const [inputText, setInputText] = useState("");
+    const [debouncedInputText] = useDebounce(inputText, 500);
+    const [zhText, setZHText] = useState([
+        {zh: "南京", pinyin: "Nan2 jing1", visible: true},
+        {zh: "林业", pinyin: "lin2 ye4", visible: true},
+        {zh: "大学", pinyin: "da4 xue2", visible: true},
+    ]);
+    useEffect(() => {
+        GET()
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                console.log("Data", data.data.data);
+                setGet(data.data.data);
+            })
+            .catch((err) => console.log("Err", err));
+    }, []);
 
-  useEffect(() => {
-    GET()
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log("Data", data.data.data);
-        setGet(data.data.data);
-      })
-      .catch((err) => console.log("Err", err));
-  }, []);
+    useEffect(() => {
+        console.log("Get", get);
+    }, [get]);
 
-  useEffect(() => {
-    console.log("Get", get);
-  }, [get]);
+    useEffect(() => {
+        //TODO: Fetch data, then set zhText
+        setinputLength(debouncedInputText.length);
+    }, [debouncedInputText]);
 
-  useEffect(() => {
-    //TODO: Fetch data, then set zhText
-    setinputLength(debouncedInputText.length);
-  }, [debouncedInputText]);
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between l:p-24">
-      <section className="h-3/5 min-h-[10rem] w-full flex justify-center flex-grow bg-gray-800 p-4 overflow-y-scroll">
-        <div className="flex flex-wrap">
-          {/*TODO: Adjust visibility based on mode*/}
-          {zhText.map((item, i) => (
-            <ZHChar
-              zh={item.zh}
-              pinyin={item.pinyin}
-              is_visible={item.visible}
-              key={i}
-            />
-          ))}
-        </div>
-      </section>
-      <section className="flex flex-col flex-grow h-2/5 min-h-[8rem] w-full">
-        <div className="flex text-white text-lg bg-blue-900 items-center py-1 px-2">
-          <div className="">
-            <select name="Preset" id="" className="bg-gray-800 p-1 text-white">
-              <option value="" disabled hidden>
-                Presets
-              </option>
-              <option value="">HSK 1</option>
-              <option value="">HSK 2</option>
-              <option value="">Intermediate</option>
-            </select>
-          </div>
-          <div className="flex flex-row-reverse flex-grow">
-            {visibilityMode.map((x, i) => (
-              <label
-                className={`flex gap-2 py-1 px-2 hover:bg-gray-400 hover:cursor-pointer ${
-                  curVisibility == x.key
-                    ? "bg-gray-200 text-black font-bold"
-                    : ""
-                }`}
-                key={i}
-              >
-                <input
-                  type="radio"
-                  name="visibility-toggle"
-                  onChange={() => setCurVisibility(x.key)}
-                  hidden={true}
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between l:p-24">
+            <section
+                className="h-3/5 min-h-[10rem] w-full flex justify-center flex-grow bg-gray-800 p-4 overflow-y-scroll">
+                <div className="flex flex-wrap">
+                    {/*TODO: Adjust visibility based on mode*/}
+                    {zhText.map((item, i) => (
+                        <ZHChar
+                            zh={item.zh}
+                            pinyin={item.pinyin}
+                            is_visible={item.visible}
+                            key={i}
+                        />
+                    ))}
+                </div>
+            </section>
+            <section className="flex flex-col flex-grow h-2/5 min-h-[8rem] w-full">
+                <div className="flex text-white text-lg bg-blue-900 items-center py-1 px-2">
+                    <div className="">
+                        <select name="Preset" id="" className="bg-gray-800 p-1 text-white">
+                            <option value="" disabled hidden>
+                                Presets
+                            </option>
+                            <option value="">HSK 1</option>
+                            <option value="">HSK 2</option>
+                            <option value="">Intermediate</option>
+                        </select>
+                    </div>
+                    <div className="flex flex-row-reverse flex-grow">
+                        {visibilityMode.map((x, i) => (
+                            <label
+                                className={`flex gap-2 py-1 px-2 hover:bg-gray-400 hover:cursor-pointer ${
+                                    curVisibility == x.key
+                                        ? "bg-gray-200 text-black font-bold"
+                                        : ""
+                                }`}
+                                key={i}
+                            >
+                                <input
+                                    type="radio"
+                                    name="visibility-toggle"
+                                    onChange={() => setCurVisibility(x.key)}
+                                    hidden={true}
+                                />
+                                <span>{x.label}</span>
+                            </label>
+                        ))}
+                    </div>
+                </div>
+                <textarea
+                    className="flex-grow w-full bg-gray-900 p-4"
+                    placeholder="Write something here"
+                    defaultValue={inputText}
+                    onChange={(e) => setInputText(e.target.value)}
                 />
-                <span>{x.label}</span>
-              </label>
-            ))}
-          </div>
-        </div>
-        <textarea
-          className="flex-grow w-full bg-gray-900 p-4"
-          placeholder="Write something here"
-          defaultValue={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-        />
-      </section>
-    </main>
-  );
+            </section>
+        </main>
+    );
 }
--- src/components/ZHChar.tsx
+++ src/components/ZHChar.tsx
@@ -1,3 +1,5 @@
+import {mark, parsePinyin} from '../utils/pinyin'
+
 function ColoredZH(props: { zh: string, tone: number }) {
     // TODO: Pick better colors
     const colors = [
@@ -5,17 +7,20 @@ function ColoredZH(props: { zh: string, tone: number }) {
     ];
     return (
         <>
-           <span style={{color: colors[props.tone - 1]}}>
-               {props.zh}
-           </span>
+            <span style={{color: colors[props.tone - 1]}}>
+    {props.zh}
+    </span>
         </>
     )
 }
 
 export function ZHChar(props: { zh: string, pinyin: string, is_visible: boolean }) {
     const zhs = props.zh.split("");
-    const numbered_pinyin = props.pinyin.split(" ");
-    const tones = numbered_pinyin.map(x => parseInt(x.slice(-1)));
+    const numbered_pinyin = props.pinyin.split(" ").map(parsePinyin);
+    const tones = numbered_pinyin.map(x => x[1]);
+    const marked_pinyin = numbered_pinyin
+        .map(x => mark(x[0], x[1]))
+        .join("");
 
     if (zhs.length != numbered_pinyin.length) {
         throw new Error("Number of character doesnt match the pinyin")
@@ -23,14 +28,14 @@ export function ZHChar(props: { zh: string, pinyin: string, is_visible: boolean 
 
     return (
         <>
-            <div className="flex flex-col items-center justify-center px-2">
+            <div className="flex flex-col items-center justify-center px-1.5">
                 <div className="text-4xl">
                     {zhs.map((zh, i) => (
                         <ColoredZH zh={zh} tone={tones[i]} key={i}/>
                     ))}
                 </div>
                 <span className={`text-lg ${props.is_visible ? "visible" : "invisible"}`}>
-                    {props.pinyin}
+                    {marked_pinyin}
                 </span>
             </div>
         </>
