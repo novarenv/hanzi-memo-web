fc2ea2a09cfcd2c42cf4904b270530933ecf0b3a
novarenv <novarenv@gmail.com>
2023-11-29 21:34:03 +0800

    PR Basic Responsive Layout conflict, add get to Page.tsx

---
+++ src/app/api/route.tsx
@@ -0,0 +1,20 @@
+export const dynamic = "force-dynamic"; // defaults to force-static
+
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function GET() {
+  const res = await fetch("https://hz.youcantry.me/api/pinyins/爱", {
+    headers: corsHeaders
+  });
+  const data = await res.json();
+
+  console.log("Res", data)
+
+  return Response.json(
+    { data },
+  );
+}
