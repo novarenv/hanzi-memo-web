19c7ae422763f9182d771a8cd777005d2487dbc7
gumelarme <gumelar.pn@gmail.com>
2023-11-29 19:44:45 +0800

    Feat: ZHChar can render a lexeme, instead of a single char

--- src/components/ZHChar.tsx
+++ src/components/ZHChar.tsx
@@ -1,25 +1,38 @@
-export function ZHChar(props: {
-  zh: string;
-  pinyin: string;
-  is_visible: boolean;
-}) {
-  // TODO: Change the behaviour so that it shows lexeme as unit, instead of each char
-  // Example:
-  //   美国人     vs   美   国   人
-  // meiguoren       mei  guo  ren
+function ColoredZH(props: { zh: string, tone: number }) {
+    // TODO: Pick better colors
+    const colors = [
+        "red", "limegreen", "rebeccapurple", "purple", "slategray"
+    ];
+    return (
+        <>
+           <span style={{color: colors[props.tone - 1]}}>
+               {props.zh}
+           </span>
+        </>
+    )
+}
 
-  // TODO: Pick better colors
-  const colors = ["red", "limegreen", "rebeccapurple", "purple", "slategray"];
+export function ZHChar(props: { zh: string, pinyin: string, is_visible: boolean }) {
+    const zhs = props.zh.split("");
+    const numbered_pinyin = props.pinyin.split(" ");
+    const tones = numbered_pinyin.map(x => parseInt(x.slice(-1)));
 
-  const tone = parseInt(props.pinyin.slice(-1));
-  const color = colors[tone - 1];
+    if (zhs.length != numbered_pinyin.length) {
+        throw new Error("Number of character doesnt match the pinyin")
+    }
 
-  return (
-    <div className="flex flex-col items-center justify-center px-2">
-      <span style={{ color }}>{props.zh}</span>
-      <span className={props.is_visible ? "visible" : "invisible"}>
-        {props.pinyin.slice(0, -1)}
-      </span>
-    </div>
-  );
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center px-2">
+                <div className="text-4xl">
+                    {zhs.map((zh, i) => (
+                        <ColoredZH zh={zh} tone={tones[i]} key={i}/>
+                    ))}
+                </div>
+                <span className={`text-lg ${props.is_visible ? "visible" : "invisible"}`}>
+                    {props.pinyin}
+                </span>
+            </div>
+        </>
+    );
 }
