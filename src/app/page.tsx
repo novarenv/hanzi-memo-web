82087a2d3328b7ba8436e5d3e87db89d63af8731
novarenv <novarenv@gmail.com>
2023-11-29 21:56:31 +0800

    Readjust Page.tsx with ZHChar Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
   const [curVisibility, setCurVisibility] = useState(visibilityMode[1].key);
   const [inputText, setInputText] = useState("");
   const [debouncedInputText] = useDebounce(inputText, 500);
+  const [inputLength, setinputLength] = useState(1);
   const [zhText, setZHText] = useState([
     { zh: "你", pinyin: "ni3", visible: true },
   ]);
@@ -31,8 +32,8 @@ export default function Home() {
         return res.json();
       })
       .then((data) => {
-        console.log("Data", data.data.data)
-        setGet(data.data.data)
+        console.log("Data", data.data.data);
+        setGet(data.data.data);
       })
       .catch((err) => console.log("Err", err));
   }, []);
@@ -43,136 +44,64 @@ export default function Home() {
 
   useEffect(() => {
     //TODO: Fetch data, then set zhText
+    setinputLength(debouncedInputText.length);
   }, [debouncedInputText]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between l:p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div
-          className="flex flex-end h-48 w-full items-end justify-center bg-gradient-to-t from-white
-            via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"
-        >
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {/* <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            /> */}
-            <div>Show</div>
-            <div>Collections</div>
-          </a>
+      <section className="h-3/5 min-h-[10rem] w-full flex justify-center flex-grow bg-gray-800 p-4 overflow-y-scroll">
+        <div className="flex flex-wrap">
+          {/*TODO: Adjust visibility based on mode*/}
+          {zhText.map((item, i) => (
+            <ZHChar
+              zh={item.zh}
+              pinyin={item.pinyin}
+              is_visible={item.visible}
+              key={i}
+            />
+          ))}
         </div>
-      </div>
-
-      {/* <div
-        className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px]
-          before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent
-          before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3
-          after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br
-          before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff]
-          after:dark:opacity-40 before:lg:h-[360px] z-[-1]"
-      ></div> */}
-
-      <div>
-        <div className="mb-6">
-          <p
-            className="flex flex-col w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200
-                pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto
-                rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 mb-4"
-          >
-            {/* <p className=""> */}
-            <code className={`text-2xl ${textShownFont}`}>郑</code>
-            <code className={`text-l ${textShownFont}`}>zheng</code>
-            {/* </p> */}
-          </p>
-          <input
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
-                focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-                dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border-blue-1000"
-            placeholder="john.doe@company.com"
-            required
-          />
+      </section>
+      <section className="flex flex-col flex-grow h-2/5 min-h-[8rem] w-full">
+        <div className="flex text-white text-lg bg-blue-900 items-center py-1 px-2">
+          <div className="">
+            <select name="Preset" id="" className="bg-gray-800 p-1 text-white">
+              <option value="" disabled hidden>
+                Presets
+              </option>
+              <option value="">HSK 1</option>
+              <option value="">HSK 2</option>
+              <option value="">Intermediate</option>
+            </select>
+          </div>
+          <div className="flex flex-row-reverse flex-grow">
+            {visibilityMode.map((x, i) => (
+              <label
+                className={`flex gap-2 py-1 px-2 hover:bg-gray-400 hover:cursor-pointer ${
+                  curVisibility == x.key
+                    ? "bg-gray-200 text-black font-bold"
+                    : ""
+                }`}
+                key={i}
+              >
+                <input
+                  type="radio"
+                  name="visibility-toggle"
+                  onChange={() => setCurVisibility(x.key)}
+                  hidden={true}
+                />
+                <span>{x.label}</span>
+              </label>
+            ))}
+          </div>
         </div>
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <textarea
+          className="flex-grow w-full bg-gray-900 p-4"
+          placeholder="Write something here"
+          defaultValue={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+      </section>
     </main>
   );
 }
