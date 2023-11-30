afc01010ed65f41093c82d0b78bc31415aa27f8a
gumelarme <gumelar.pn@gmail.com>
2023-12-01 01:19:26 +0800

    Fix: spacing causes pinyin parsing error

--- src/components/ZHChar.tsx
+++ src/components/ZHChar.tsx
@@ -2,57 +2,54 @@ import {mark, parsePinyin} from '@/utils/pinyin'
 import {useEffect, useState} from "react";
 
 function ColoredZH(props: { zh: string, tone: number }) {
-    // TODO: Pick better colors
-    const colors = [
-        "red", "limegreen", "rebeccapurple", "purple", "slategray"
-    ];
-    return (
-        <>
+  // TODO: Pick better colors
+  const colors = [
+    "red", "limegreen", "rebeccapurple", "purple", "slategray"
+  ];
+  return (
+      <>
             <span style={{color: colors[props.tone - 1]}}>
     {props.zh}
     </span>
-        </>
-    )
+      </>
+  )
 }
 
 export function ZHChar(props: { zh: string, pinyin: string, is_visible: boolean }) {
-    const zhs = props.zh.split("");
-    const numbered_pinyin = props.pinyin.split(" ").map(parsePinyin);
-    const tones = numbered_pinyin.map(x => x[1]);
-    const marked_pinyin = numbered_pinyin
-        .map(x => mark(x[0], x[1]))
-        .join("");
+  const zhs = props.zh.split("");
+  const numbered_pinyin = props.pinyin.split(" ").map(parsePinyin);
+  const tones = numbered_pinyin.map(x => x[1]);
+  const marked_pinyin = numbered_pinyin
+      .map(x => mark(x[0], x[1]))
+      .join("");
 
-    const [show, setShow] = useState(props.is_visible);
+  const [show, setShow] = useState(props.is_visible);
 
-    if (zhs.length != numbered_pinyin.length) {
-        throw new Error("Number of character doesnt match the pinyin")
-    }
+  function toggleVisiblity(value: boolean) {
+    setShow(props.is_visible || value)
+  }
 
-    function toggleVisiblity(value: boolean){
-        setShow(props.is_visible || value)
-    }
+  useEffect(() => {
+    toggleVisiblity(props.is_visible)
+  }, [props.is_visible])
 
-    useEffect(() => {
-        toggleVisiblity(props.is_visible)
-    }, [props.is_visible])
-
-    return (
-        <>
-            <div className="flex flex-col items-center justify-center px-1.5 gap-1.5"
-                 onMouseOver={() => toggleVisiblity( true)}
-                 onMouseLeave={() => toggleVisiblity( false)}>
-                <div className="text-4xl">
-                    {zhs.map((zh, i) => (
-                        <ColoredZH zh={zh} tone={tones[i]} key={i}/>
-                    ))}
-                </div>
-                <div className={`${!show ? "bg-gray-800 border border-solid border-gray-600 rounded-sm" : ""}`}>
+  return (
+      <>
+        <div className="flex flex-col items-center justify-center px-1.5 gap-1.5"
+             onMouseOver={() => toggleVisiblity(true)}
+             onMouseLeave={() => toggleVisiblity(false)}>
+          <div className="text-4xl">
+            {zhs.length == numbered_pinyin.length ?
+                zhs.map((zh, i) => (<ColoredZH zh={zh} tone={tones[i]} key={i}/>))
+                : <span>{props.zh}</span>
+            }
+          </div>
+          <div className={`${!show ? "bg-gray-800 border border-solid border-gray-600 rounded-sm" : ""}`}>
                     <span className={`text-lg  ${show ? "visible" : "invisible"}`}>
                         {marked_pinyin}
                     </span>
-                </div>
-            </div>
-        </>
-    );
+          </div>
+        </div>
+      </>
+  );
 }
